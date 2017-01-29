924f8a8bd28a7cfc7a605e7eb46ceaf83a4195e3
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-01-29 11:31:13 +0900

    読み込みファイルの\を修正

--- ui/customer/customerdetail.php
+++ ui/customer/customerdetail.php
@@ -2,7 +2,7 @@
 namespace ui\customer;
 use business\entity\Customer;
 use ui\util;
-require_once(dirname(__FILE__)."\..\staff.php");
+require_once(dirname(__FILE__)."/../staff.php");
 require_once(dirname(__FILE__)."/../util/control-util.php");
 abstract class CustomerDetail
 {
