9d27d3e200f48357eb8dfaed9593c50465255653
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-03-21 03:35:26 +0900

    途中

--- ui/sales/sales-context.php
+++ ui/sales/sales-context.php
@@ -10,6 +10,15 @@ class SalesContext
 
 	public $view_mode;
 
+	public function get_from_month()
+	{
+		return $_POST['from_month'];
+	}
+	public function get_to_month()
+	{
+		return $_POST['to_month'];
+	}
+
 	public static function get_instance() : SalesContext
 	{
 		if(is_null(self::$_instance))
