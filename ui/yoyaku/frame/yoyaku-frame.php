5d5b1984c355e3ad14200480ff9a9a8be66279cb
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-04-01 00:02:54 +0900

    予約
    不要な関数

--- ui/yoyaku/frame/yoyaku-frame.php
+++ ui/yoyaku/frame/yoyaku-frame.php
@@ -72,46 +72,5 @@ class YoyakuFrame
 	}
 }
 
-function init_yoyaku()
-{
-
-
-	function regist_css()
-	{
-		wp_register_style(
-			'yoyaku/common.css', 
-			plugins_url("/css/yoyaku/common.css", __FILE__),
-			array(),
-			"1.0"
-			 
-		);
-		
-		wp_enqueue_style('yoyaku/common.css');
-
-		wp_register_style(
-			'customer_search.css', 
-			plugins_url("/css/customer_search.css", __FILE__),
-			array(),
-			"0.006"
-			 
-		);
-		
-		wp_enqueue_style('customer_search.css');
-		
-		wp_enqueue_style('manage_common.css');
-
-		wp_register_style(
-			'manage_common.css', 
-			plugins_url("/css/manage_common.css", __FILE__),
-			array(),
-			"0.002"
-		);
-		
-		wp_enqueue_style('manage_common.css');
-	}
-
-	add_action('wp_enqueue_scripts', 'regist_css');
-
-}
 
 ?>
