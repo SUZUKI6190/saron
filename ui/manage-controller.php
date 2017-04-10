07be4737fbe8d57fb36a5e083967c866c76cd432
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-04-10 17:39:33 +0900

    管理画面
    タイトル

--- ui/manage-controller.php
+++ ui/manage-controller.php
@@ -57,7 +57,7 @@ class ManageController implements IController
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
 		<head>
-		<title></title>
+		<title>予約システム管理画面</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta name="viewport" content="width=device-width" />
 		<meta http-equiv="Pragma" content="no-cache" />
