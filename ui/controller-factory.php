da02db3b7d88c74125602595e0e37aadac97b43a
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-04-12 07:52:54 +0900

    管理FW
    インクルードのパスが不正

--- ui/controller-factory.php
+++ ui/controller-factory.php
@@ -9,7 +9,7 @@ require_once(dirname(__FILE__).'/frame/manage-frame.php');
 require_once(dirname(__FILE__).'/frame/result.php');
 require_once('manage-frame-implementer-factory.php');
 require_once(dirname(__FILE__).'/yoyaku/yoyaku-controller.php');
-require_once(dirname(__FILE__).'../image/download-image.php');
+require_once(dirname(__FILE__).'/image/download-image.php');
 
 function create_controller() : IController
 {
