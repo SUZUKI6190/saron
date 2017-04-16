6deddef9c4edd48724d2bd12c5c75068be84ad1b
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-04-17 05:29:37 +0900

    スタッフ管理
    画像アップロードの切り替え

--- js/manage-staff.js
+++ js/manage-staff.js
@@ -1,5 +1,17 @@
-function onclick_upload(id)
+function onclick_upload(id, textid)
 {
 	var area = document.getElementById(id);
-	area.className = 'image_upload_area';
+	var css_default = 'image_upload_area close';
+	var css_open = 'image_upload_area';
+	var text_area = document.getElementById(textid);
+	if(area.className == css_default){
+		area.className = css_open;
+		text_area.innerHTML = "アップロードを取り消す";
+	}else{
+		var old = area.innerHTML;
+		area.innerHTML = old;
+		area.className = css_default;
+		text_area.innerHTML = "画像をアップロードする";
+	}
+	
 }
