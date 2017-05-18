d81635f1db03276a0ddde38c4033207e9bd8f931
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-05-18 17:15:39 +0900

    日時指定
    postデータ値

--- ui/yoyaku/controll/yoyaku-button.php
+++ ui/yoyaku/controll/yoyaku-button.php
@@ -6,26 +6,17 @@ class YoyakuToggle
 {
     public $enable_yoyaku = false;
     private $_staff_id;
-    private $_date_time;
-    private $_input;
+    private $_value;
 
     public function __construct(\DateTime $d)
     {
-        $name= $d->format('YMdHi');
-        $this->_input = new InputControll('submit', $name);
+        $this->_value= $d->format('Y-m-d H:i:s');
     }
 
     public function view()
     {
-        $add_css = "";
-        $atr = [];
         if($this->enable_yoyaku){
-            $this->_input->set_value("◎");
-            $add_css = "enable";
-            $css = "yoyaku_button $add_css";
-            $this->_input->set_style($css);
-            $this->_input->set_attribute($atr);
-            $this->_input->view();
+            echo "<button type='submit' class='yoyaku_button' name='select_time' value='$this->_value'>◎</button>";
         }else{
             echo "<span class='disable_cell'>✕</span>";
         }
