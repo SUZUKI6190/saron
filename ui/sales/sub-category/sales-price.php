ccb5f04c17ff34d1bc9f15d1017c3730b5da5acc
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-07-01 21:38:36 +0900

    売り上げ管理

--- ui/sales/sub-category/sales-price.php
+++ ui/sales/sub-category/sales-price.php
@@ -13,6 +13,18 @@ use ui\sales\salesContext;
 class SalesPriceSub extends SalesSubBase
 {
 	private $_form_id = "menu_form";
+	const MainColor = "192";
+	const OtherColor = "75";
+	const Alpha = "0.5";
+	const YearsColor = "rgba(192, 75 , 75, 0.4)";
+	const LastYearsAgoColor = "rgba(75, 192 , 75, 0.4)";
+	const ThreeYearsAgoColor = "rgba(75, 75 , 192, 0.4)";
+	
+	const ColorTable = [
+		self::ThreeYearsAgoColor,
+		self::LastYearsAgoColor,
+		self::YearsColor
+	];
 
 	protected function create_monthly_graph_param() : GraphData
 	{
@@ -25,11 +37,15 @@ class SalesPriceSub extends SalesSubBase
 		}
 		$yealy_list = \business\facade\get_yoyaku_registration_last_3_years();
 		$year_list = array_keys($yealy_list);
+		$counter = 0;
 		foreach($year_list as $year)
 		{
 			$yr_list = $yealy_list[$year];
 			$new_dataset = new DataSet();
 			$new_dataset->label = $year;
+			$new_dataset->backgroundColor = self::ColorTable[$counter];
+			$new_dataset->borderColor = self::ColorTable[$counter];
+
 			foreach($yr_list as $y){
 				$reserved_course = \business\facade\get_reserved_course_by_registration_id($y->id);
 				foreach($reserved_course as $rc)
@@ -38,6 +54,7 @@ class SalesPriceSub extends SalesSubBase
 				}
 			}
 			$ret->dataset_list[] = $new_dataset;
+			$counter++;
 		}
 		return $ret;
 	}
