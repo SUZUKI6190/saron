4d2ff072c46cafb51b7126b34eb6f35e4e905c8a
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-07-06 07:47:38 +0900

    お客様管理フォルダ構成

---
+++ ui/customer/sub-category/customer-sub-cotegory.php
@@ -0,0 +1,29 @@
+<?php
+namespace ui\customer;
+use \SplFileObject;
+use \business\entity\Config;
+use \business\facade;
+require_once('customerdetail.php');
+require_once('customerDetailNew.php');
+require_once('customerDetailEdit.php');
+require_once(dirname(__FILE__).'/search/search-sub.php');
+require_once(dirname(__FILE__).'/regist-new/regist-new-sub.php');
+require_once(dirname(__FILE__).'/delete/delete-sub.php');
+require_once(dirname(__FILE__).'/mass-registration/mass-registration-sub.php');
+
+abstract class CustomerSubBase extends \ui\frame\SubCategory
+{
+	protected $_context;
+	public function __construct(ControlContext $context)
+	{
+		$this->_context = $context;
+	}
+
+	public function init()
+	{
+		
+	}
+}
+
+
+?>
