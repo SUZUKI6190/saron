db424efbce43a84f6187e825e487eb1f3224cf8f
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-07-21 17:20:34 +0900

    プラグイン設定画面

--- plugin-menu/create-menu.php
+++ plugin-menu/create-menu.php
@@ -3,7 +3,9 @@
 class PluginConroller
 {
     private function __construct(){}
- 
+    const SettingGroup = "YoyakuSettingGroup";
+    const UrlName = 'YoyakuUrlName';
+
     /** ステップ1 */
     public static function my_plugin_menu() {
         add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'PluginConroller::my_plugin_options' );
@@ -21,19 +23,38 @@ class PluginConroller
 
     //管理画面にメニューを追加
     public static function add_pages(){
-        add_menu_page('予約システム管理', '予約システム管理', 'level_8', __FILE__, 'PluginConroller::csv_upload', 'dashicons-upload',26);
+        add_menu_page('予約システム管理', '予約システム管理', 'level_8', __FILE__, 'PluginConroller::view_setup', 'dashicons-upload',26);
     }
     
    //プラグインの表示
-    public static function csv_upload(){
-    ?>
+    public static function view_setup(){
+?>
+
+<form method="post" action="options.php">
+<?php 
+settings_fields(self::SettingGroup);
+do_settings_sections(self::SettingGroup);
+?>
+<?php submit_button(); ?>
+    <div>
+        <span>予約システムの先頭URL名:<span>
+        <input type='text' name='<?php echo self::UrlName; ?>' value='<?php echo get_option(self::UrlName); ?>'>
+    <div>
+</form>
+
     <?php
+    }
+
+    public static function register_settings() {
+        register_setting( self::SettingGroup , self::UrlName );
     }
 
     public static function run()
     {
+        add_action('admin_init', 'PluginConroller::register_settings' );
+    
         /** 上のテキストのステップ2 */
-        add_action( 'admin_menu', 'PluginConroller::my_plugin_menu' );
+        add_action('admin_menu', 'PluginConroller::my_plugin_menu' );
      
         // 管理メニューに追加するフック
         add_action('admin_menu', 'PluginConroller::add_pages');
