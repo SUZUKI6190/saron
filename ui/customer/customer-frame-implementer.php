17519d011ab6b0e8dbffc919cb63e30a2bb57aa4
SUZUKI6190 <harp6662002@yahoo.co.jp>
2017-08-19 05:03:17 +0900

    スケジュール管理

--- ui/customer/customer-frame-implementer.php
+++ ui/customer/customer-frame-implementer.php
@@ -2,7 +2,7 @@
 namespace ui\customer;
 require_once(dirname(__FILE__).'/../../business/facade/customer.php');
 require_once(dirname(__FILE__).'/../../business/entity/customer.php');
-require_once(dirname(__FILE__).'/sub-category/customer-sub-cotegory.php');
+require_once(dirname(__FILE__).'/sub-category/customer-sub-base.php');
 require_once(dirname(__FILE__).'/customer-context.php');
 require_once(dirname(__FILE__).'/../frame/manage-frame.php');
 require_once(dirname(__FILE__).'/../../business/facade/customer.php');
